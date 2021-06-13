a60c5488254e3a3c37ded6358b4a088abe5ce7ce
KuzoryaAky <opanforever1992@gmail.com>
2021-06-13 21:36:46 +0300

    ConverterJsonToTxt

---
+++ RequestToTheInformationGuide/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Text.Json;
+using System.IO;
+using System.Collections.Generic;
+
+namespace ConverterJsonToTxt
+{
+    class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            string _pathToRead = $"C:\\Users\\opanf\\OneDrive\\Рабочий стол\\Users.json";
+            string _pathToWrite = $"C:\\Users\\opanf\\OneDrive\\Рабочий стол\\UserConverted.txt";
+            try
+            {
+                string jsonFromFile;
+                using (var reader = new StreamReader(_pathToRead))
+                {
+                    Console.WriteLine(jsonFromFile = reader.ReadToEnd());
+                    File.WriteAllText(_pathToWrite, jsonFromFile);
+                }  
+            }
+            
+            
+            
+        }
+
+    }
+}
